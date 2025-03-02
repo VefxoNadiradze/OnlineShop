c8c2d893926f8f00f384a616db6294f8065d41b1
VefxoNadiradze <olld00@bk.ru>
2025-03-02 13:22:08 +0400

    initial state

---
+++ src/redux/Data.ts
@@ -0,0 +1,54 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const fetchProducts = createAsyncThunk<Product[], void>(
+  "products/fetchProducts",
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await fetch(`https://dummyjson.com/products?limit=${10}`);
+      if (!response.ok) {
+        throw new Error("Failed to fetch products");
+      }
+      const data = await response.json();
+      return data.products; 
+    } catch (error: any) {
+
+      return rejectWithValue(error.message || "Failed to fetch products");
+    }
+  }
+);
+
+interface ProductState {
+  products: Product[];
+  loading: boolean;
+  error: string | null;
+}
+
+const initialState: ProductState = {
+  products: [],
+  loading: false,
+  error: null,
+};
+
+const productSlice = createSlice({
+  name: "products",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchProducts.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchProducts.fulfilled, (state, action) => {
+        state.loading = false;
+        state.products = action.payload;
+      })
+      .addCase(fetchProducts.rejected, (state, action) => {
+        state.loading = false;
+
+        state.error = action.payload as string || "Failed to fetch products";
+      });
+  },
+});
+
+export default productSlice.reducer;
