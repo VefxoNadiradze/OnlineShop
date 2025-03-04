41faf4b5d29832d9b437e59d89606c17d3dc13fb
VefxoNadiradze <olld00@bk.ru>
2025-03-04 17:54:22 +0400

    create navigation and some components. finish desktop styles for header

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,42 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './components/Layout'
+import { Provider } from 'react-redux'
+import { Store } from './redux/store'
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import About from './pages/About'
+import GlobalStyle from './GlobalStyles'
 
 
-
+const router = createBrowserRouter([
+   {
+     path: "/",
+     element: <Layout/>,
+     children: [
+        {
+          path: "/",
+          element: <Home/>
+        },
+        {
+          path: "/Contact",
+          element: <Contact/>
+        },
+        {
+          path: "/About",
+          element: <About/>
+        }
+     ]
+   }
+])
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-
+    <Provider store={Store}>
+      <GlobalStyle/>
+    <RouterProvider router={router} />
+    </Provider>
   </StrictMode>
 )
