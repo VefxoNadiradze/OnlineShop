23cec402729f3369b7d7023af1b210d94f6d69ef
VefxoNadiradze <olld00@bk.ru>
2025-04-01 17:42:38 +0400

    add contact items

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,21 @@
-
+import styled from "styled-components";
 
 export default function Contact() {
-  return (
-    <div>Contact</div>
-  )
+  return <ContactParent>
+      <div className="contactItems">
+          <div className="callItem"></div>
+          <div className="messageItem"></div>
+      </div>
+     <form >
+        <div className="smallInputs">
+
+        </div>
+        <input type="message" />
+     </form>
+     
+  </ContactParent>;
 }
+
+const ContactParent = styled.div`
+  display: flex;
+`;
