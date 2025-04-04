b3413f35f8d080fb09bc435588f5b1f1228c7da9
VefxoNadiradze <olld00@bk.ru>
2025-04-04 20:24:21 +0400

    remove quantity slice from store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productSlice from "./Data"
 import cartSlice from "./Cart"
-import quantitySlice from "./quantity"
-
 export const Store = configureStore({
      reducer: {
         data: productSlice,
-        cartData: cartSlice,
-        quantity: quantitySlice
+        cartData: cartSlice
      }
 })
 
