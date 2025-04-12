d7b9f24f7a621610866b92ff09bd200119c43728
VefxoNadiradze <olld00@bk.ru>
2025-04-12 11:45:34 +0400

    add responsive styles on the contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -47,6 +47,13 @@ const ContactParent = styled.div`
   align-items: center;
   margin: 140px 0;
   gap: 30px;
+  @media screen and (max-width: 995px) {
+    margin: 50px 0;
+  }
+
+  @media screen and (max-width: 1300px) {
+    flex-direction: column-reverse;
+  }
 
   .contactItems {
     height: 457px;
@@ -57,6 +64,10 @@ const ContactParent = styled.div`
     box-shadow: 0px 1px 13px 0px rgba(0, 0, 0, 0.05);
     background: rgb(255, 255, 255);
     padding: 40px 35px;
+    @media screen and (max-width: 1300px) {
+      width: 100%;
+      height: auto;
+    }
     div {
       .iconTitle {
         display: flex;
@@ -84,23 +95,48 @@ const ContactParent = styled.div`
         font-weight: 400;
         line-height: 21px;
         margin-bottom: 10px;
+        @media screen and (max-width: 1300px) {
+          font-size: 13px;
+        }
       }
     }
   }
 
   .formPar {
+    width: 100%;
     padding: 40px 31px;
     border-radius: 4px;
     box-shadow: 0px 1px 13px 0px rgba(0, 0, 0, 0.05);
     background: rgb(255, 255, 255);
+    @media screen and (max-width: 1300px) {
+      display: flex;
+      max-width: 100%;
+      justify-content: center;
+    }
+    @media screen and (max-width: 800px) {
+      width: 800px !important;
+    }
+
+    @media screen and (max-width: 550px) {
+      padding: 30px 20px;
+    }
+    @media screen and (max-width: 380px) {
+      padding: 20px 10px;
+    }
     form {
+      width: 100%;
+
       .smallInputs {
         display: flex;
         column-gap: 16px;
+        @media screen and (max-width: 995px) {
+          flex-direction: column;
+          gap: 20px;
+        }
 
         input {
-          width: 235px;
           height: 50px;
+          width: 235px;
           border-radius: 4px;
           background: rgb(245, 245, 245);
           font-family: "Poppins", sans-serif;
@@ -110,38 +146,52 @@ const ContactParent = styled.div`
           line-height: 24px;
           border: none;
           outline: none;
+
+          @media screen and (max-width: 1300px) {
+            width: 100% !important;
+          }
         }
       }
-      .TextArea {
-        width: 737px !important;
-        height: 207px !important;
-        border-radius: 4px;
-        background: rgb(245, 245, 245);
-        padding: 13px 16px;
-        margin-top: 32px;
-        color: rgb(0, 0, 0);
-        font-family: "Poppins", sans-serif;
-        font-size: 16px;
-        font-weight: 400;
-        line-height: 24px;
-        outline: none;
-        border: none;
-      }
+    }
+    .TextArea {
+      width: 100%;
+      height: 207px !important;
+      border-radius: 4px;
+      background: rgb(245, 245, 245);
+      padding: 13px 16px;
+      margin-top: 32px;
+      color: rgb(0, 0, 0);
+      font-family: "Poppins", sans-serif;
+      font-size: 16px;
+      font-weight: 400;
+      line-height: 24px;
+      outline: none;
+      border: none;
 
-      button {
-        display: block;
-        border-radius: 4px;
-        background: rgb(219, 68, 68);
-        color: rgb(250, 250, 250);
-        font-family: "Poppins", sans-serif;
-        font-size: 16px;
-        font-weight: 500;
-        line-height: 24px;
-        padding: 16px 48px;
-        cursor: pointer;
-        margin-top: 32px;
-        margin-left: auto;
-        border: none;
+      @media screen and (max-width: 1300px) {
+        width: 100% !important;
+      }
+    }
+
+    button {
+      display: block;
+      border-radius: 4px;
+      background: rgb(219, 68, 68);
+      color: rgb(250, 250, 250);
+      font-family: "Poppins", sans-serif;
+      font-size: 16px;
+      font-weight: 500;
+      line-height: 24px;
+      padding: 16px 48px;
+      cursor: pointer;
+      margin-top: 32px;
+      margin-left: auto;
+      border: none;
+
+      @media screen and (max-width: 995px) {
+        font-size: 14px;
+        line-height: 20px;
+        padding: 13px 30px;
       }
     }
   }
