034fbd806d160e6370e0c68f99df65e4af7edd4a
VefxoNadiradze <olld00@bk.ru>
2025-04-19 13:09:04 +0400

    add responsive styles on the about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -69,13 +69,27 @@ const AboutParent = styled.div`
   .ourStory {
     margin-top: 42px;
     height: 609px;
+    @media screen and (max-width: 1100px) {
+      height: auto;
+    }
     .textImgPar {
       position: absolute;
       display: flex;
       align-items: center;
       gap: 77px;
+      @media screen and (max-width: 1200px) {
+        gap: 70px;
+      }
 
+      @media screen and (max-width: 1100px) {
+        position: static;
+        flex-direction: column;
+        gap: 50px;
+      }
       .storyText {
+        @media screen and (max-width: 1100px) {
+          text-align: center;
+        }
         h3 {
           color: rgb(0, 0, 0);
           font-family: "Inter", sans-serif;
@@ -84,6 +98,14 @@ const AboutParent = styled.div`
           line-height: 64px;
           letter-spacing: 6%;
           margin-bottom: 40px;
+          @media screen and (max-width: 1200px) {
+            font-size: 43px;
+            margin-bottom: 30px;
+          }
+          @media screen and (max-width: 670px) {
+            font-size: 30px;
+            margin-bottom: 10px;
+          }
         }
 
         p {
@@ -92,21 +114,53 @@ const AboutParent = styled.div`
           font-size: 16px;
           font-weight: 400;
           line-height: 26px;
+          @media screen and (max-width: 1200px) {
+            font-size: 14px;
+          }
+          @media screen and (max-width: 670px) {
+            font-size: 12px;
+            line-height: 18px;
+          }
         }
       }
 
       img {
         width: 737px;
         height: 519px;
+        object-fit: cover;
+
+        @media screen and (max-width: 1200px) {
+          width: 637px;
+          height: 419px;
+        }
+        @media screen and (max-width: 670px) {
+          width: 437px;
+          height: 319px;
+          object-fit: contain;
+        }
+
+        @media screen and (max-width: 460px) {
+          width: 283px;
+          height: 200px;
+        }
       }
     }
   }
 
   .cardsParent {
+    margin-top: 77px;
     display: grid;
     grid-template-columns: repeat(4, 1fr);
     gap: 34px;
 
+    @media screen and (max-width: 1200px) {
+      grid-template-columns: repeat(2, 1fr);
+      gap: 25px;
+    }
+    @media screen and (max-width: 670px) {
+      grid-template-columns: repeat(1, 1fr);
+      gap: 20px;
+    }
     .card {
       text-align: center;
       box-sizing: border-box;
@@ -125,6 +179,11 @@ const AboutParent = styled.div`
         background: rgb(0, 0, 0);
         color: rgb(250, 250, 250);
         font-size: 27px;
+        @media screen and (max-width: 670px) {
+          width: 48px;
+          height: 48px;
+          font-size: 20px;
+        }
       }
 
       .texts {
@@ -137,6 +196,9 @@ const AboutParent = styled.div`
           font-weight: 700;
           line-height: 30px;
           letter-spacing: 4%;
+          @media screen and (max-width: 670px) {
+            font-size: 22px;
+          }
         }
 
         p {
@@ -145,6 +207,9 @@ const AboutParent = styled.div`
           font-weight: 400;
           line-height: 24px;
           margin-top: 12px;
+          @media screen and (max-width: 670px) {
+            font-size: 14px;
+          }
         }
       }
     }
